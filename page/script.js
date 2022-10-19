9abe8e0b9bb010275f17f6b340969d7b79a63fe5
Akash Dandriyal <akash.d1409@gmail.com>
2022-10-19 22:25:08 +0530

    added carlinks and ui fixes

--- page/script.js
+++ page/script.js
@@ -1,3 +1,4 @@
+let carListContainer = document.getElementById('carList');
 function getFilteredCars() {
   var fPrice = document.getElementById('fPrice').value;
   var tPrice = document.getElementById('tPrice').value;
@@ -17,6 +18,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪ 🟤',
       type: 'Hatchback',
       price: 4,
+      link: 'https://www.marutisuzuki.com/alto-k10?utm_source=google&utm_medium=cpc&utm_campaign=18478007953&utm_term=maruti%20alto%20k10&utm_content=m&gclid=EAIaIQobChMIsYye1PXp-gIVn5NmAh1Npwf2EAAYASAAEgKZUvD_BwE'
     },
     {
       car: 'Maruti Suzuki Swift (Petrol)',
@@ -24,6 +26,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'Hatchback',
       price: 5.9,
+      link: 'https://www.marutisuzuki.com/swift?form=testdrive&utm_source=google&utm_medium=cpc&utm_campaign=13868647135&utm_term=swift&utm_content=m&gclid=EAIaIQobChMInZun-vXp-gIVuINLBR29KQ_LEAAYASAAEgJJDvD_BwE'
     },
     {
       car: 'Tata Punch (Petrol)',
@@ -31,6 +34,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'SUV',
       price: 6,
+      link: 'https://cars.tatamotors.com/suv/punch'
     },
     {
       car: 'Mahindra Bolero (Petrol)',
@@ -38,6 +42,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'SUV',
       price: 10,
+      link: 'https://www.google.com/amp/s/auto.mahindra.com/suv/bolero%3famp=true'
     },
     {
       car: 'Maruti Suzuki Baleno (Petrol)',
@@ -45,6 +50,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'Hatchback',
       price: 6.5,
+      link: ''
     },
     {
       car: 'Maruti Brezza (Petrol)',
@@ -52,6 +58,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'SUV',
       price: 8,
+      link: ''
     },
     {
       car: 'Maruti S-Presso (Petrol)',
@@ -59,6 +66,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'Hatchback',
       price: 4.7,
+      link: ''
     },
     {
       car: 'Tata Nexon (Petrol)',
@@ -66,6 +74,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'SUV',
       price: 7.6,
+      link: ''
     },
     {
       car: 'Honda City (Petrol)',
@@ -73,6 +82,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'Sedan',
       price: 12,
+      link: ''
     },
     {
       car: 'Tata Safari (Petrol)',
@@ -80,6 +90,7 @@ function getFilteredCars() {
       colr: '🔴 🔵 ⚫ ⚪',
       type: 'SUV',
       price: 16,
+      link: ''
     }
     
   ];
@@ -87,7 +98,7 @@ function getFilteredCars() {
   let resultCar = [];
   let resultPrice = [];
   let j = 0;
-  let selectedCars = '';
+  let selectedCars = [];
   for (let i = 0; i < cars.length; i++) {
 
     if (
@@ -96,24 +107,23 @@ function getFilteredCars() {
       cars[i]['type'] == type &&
       cars[i]['seat'] >= minseats 
     ) {
-        resultCar[j] = cars[i]['car'];
-        resultPrice[j] = cars[i]['price'] + ' lacs ' + ' | ' + cars[i].colr;
-        j = j + 1;
+        let carObj = {
+          car: cars[i]['car'] + ' : Rs. ' + cars[i]['price'] + ' lacs ' + ' | ' + cars[i].colr,
+          link: cars[i]['link']
+        };
+        selectedCars.push(carObj);
     }
   }
-
-  for (let i = 0; i < resultCar.length - 1; i++) {
-    selectedCars = selectedCars + resultCar[i] + ' : Rs. ' + resultPrice[i] + '\n';
+  for(let i = 0; i < selectedCars.length; i++) {
+    let carLink = document.createElement('a');
+    carLink.innerText = selectedCars[i].car;
+    carLink.href = selectedCars[i].link;
+    carLink.target = '_blank';
+    carLink.classList.add('carLink', 'text-base', 'font-medium', 'text-gray-500', 'bg-gray-50', 'hover:text-gray-900', 'hover:bg-gray-100', 'dark:text-gray-400', 'dark:bg-gray-800', 'dark:hover:bg-gray-700', 'dark:hover:text-white');
+    carListContainer.appendChild(carLink);
   }
-  selectedCars =
-  selectedCars +
-    resultCar[resultCar.length - 1] +
-    ' : Rs. ' +
-    resultPrice[resultCar.length - 1];
-
-  document.querySelector('#demo').textContent = selectedCars;
 }
 
-$("button").click(function () {
-  $("#myform").get(0).reset();
-});
+function resetFields() {
+  carListContainer.innerHTML = '';
+}
